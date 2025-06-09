86cfb3c4dedb2035fa571575fc97dd6f2732f6b9
siro844 <srinathreddy239@gmail.com>
2025-06-09 13:36:47 +0530

    add name field

--- apps/http-server/src/types/auth.ts
+++ apps/http-server/src/types/auth.ts
@@ -9,6 +9,7 @@ export interface AuthenticatedRequest extends Request {
 export const UserAuth = z.object({
     email: z.string().email({ message: "Invalid Email" }),
     password: z.string().min(6, { message: "Too Short" }).max(100, { message: " Tooo Long" }).trim(),
+    name : z.string().max(20,{ message :" Less than 20 characters only"}),
 })
 
 export type User = z.infer<typeof UserAuth>
