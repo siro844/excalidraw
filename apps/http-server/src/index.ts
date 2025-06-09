c5420be91de158b9a68bbfe114d2014bbfc58a98
siro844 <srinathreddy239@gmail.com>
2025-06-09 21:32:51 +0530

    fix

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -4,7 +4,7 @@ import rootRouter from "./routes/index.js";
 const app = express();
 const PORT = 5000
 
-
+app.use(express.json())
 app.use('/api/v1',rootRouter)
 app.listen(PORT,()=>{
     console.log("SERVER IS RUNNING ON PORT" + PORT)
